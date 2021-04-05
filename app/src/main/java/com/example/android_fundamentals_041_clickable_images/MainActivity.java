b0930da2c4bc067e16f6bda811ed3c4e039bd89f
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-04-05 16:02:05 +0200

    [AF041]Basic Activty image views and discriptions
    Deleted the empty MainActivty and Layout.
    Created a new one from the Basic Activity template.
    In the content_main.xml I added three image views with content description and saved that description in res > strings.xml. The same content description is used for the text of the text view next to image view.
    The option "Match constrain" is used for the text view to match the content to the constrains.
    
    In Main Activity I added a onClick method to resolve the click of any image view thaht has the onClick attribute with the name of the method. The method has to be public void and has to take a View obj as argument.
    
    The takeOrder method then shows a Toast with a message that is initialisied wit a string res for every particular image view, that was clicked.

--- app/src/main/java/com/example/android_fundamentals_041_clickable_images/MainActivity.java
+++ app/src/main/java/com/example/android_fundamentals_041_clickable_images/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.android_fundamentals_041_clickable_images;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    /**
+     * Shows a message that one of the desserts images was clicked
+     * The message depends on the image that is clicked and the text is stored in the strings.xml
+     * @param view
+     */
+
+    public void takeOrder(View view){
+
+        String message;
+
+        switch (view.getId())
+        {
+            case R.id.iV_donut:
+                message = getString(R.string.donut_order_message);
+                break;
+            case R.id.iV_ice_cream:
+                message = getString(R.string.ice_cream_order_message);
+                break;
+            case R.id.iV_froyo:
+                message = getString((R.string.froyo_order_message));
+                break;
+            default:
+                message = "No order taken";
+        }
+
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 }
