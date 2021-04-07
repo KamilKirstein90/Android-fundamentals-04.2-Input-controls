4c78bed998ddd557f9e66d95fae7f44c0d3943b8
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-04-07 08:11:21 +0200

    [AF042] Input controls Codelabs
    Deleted not needet fragments.
    In Act2 I created multiple edit text views with different input attributes like for example  phone for the number.
    I also created a RadioGroup with three RadioButton an a onClick method that responds to the right radio button.
    The Radiobutton rb_nextday is initialized  to state chacked when the Act2 is created.

--- app/src/main/java/com/example/android_fundamentals_041_clickable_images/Act2.java
+++ app/src/main/java/com/example/android_fundamentals_041_clickable_images/Act2.java
@@ -5,20 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Act2 extends AppCompatActivity {
+// a spinner is used in this activity and for it to react to the selection of it items a AdapterView.OnItemSelectedListener
+// interface must be implemented and with it the two methods the onItemSelected and onNothingSelected
+
+public class Act2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final String SAVE_BUNDLE_ORDER_MESSAGE = "Act2.bundle.string.mOrderMessageFromAct1";
     private String mOrderMessageFromAct1;
     TextView tv_orderMessage;
+    RadioButton rbNextDay;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act2);
         tv_orderMessage = findViewById(R.id.tV_orderMessage);
+        rbNextDay = findViewById(R.id.rb_nextday);
+         //initialize next day button
+        rbNextDay.setChecked(true);
+
+        // look for the spinner if not null set the onItemSeletedListeoner of this context to it
+
+        spinner = findViewById(R.id.spinner);
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears. this is a already available layout from the android library for one spinner item
+
+        if (spinner != null)
+        {
+            spinner.setOnItemSelectedListener(this);
+            // the listener of this spinner is now this act2 and it can response to it in the onItemSelected method
+
+            // set the adapter to it
+            spinner.setAdapter(adapter);
+
+        }
+
+
+
         // get the intent from MainAct
         if (savedInstanceState != null)
         {
@@ -45,4 +80,48 @@ public class Act2 extends AppCompatActivity {
 
     }
 
+    public void onRadioButtonClicked(View view) {
+
+        // for radio button the checked state has to be checked as well
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId())
+        {
+            case R.id.rb_sameday:
+                if(checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.rb_nextday:
+                if(checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.rb_pickup:
+                if(checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+        }
+
+    }
+
+    /**
+     * create an easier method for displaying tost messages
+     * @param message
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel;
+        spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
